7b2547979affedd9bdf818ab59ebd7ee5dc7a3c6
Diogo Schutz <diogoschutz1@gmail.com>
2022-03-27 19:33:36 -0300

    segundo commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,31 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
-import { Container } from './styles'
-import { Content } from './styles'
+
+import { Container, Content } from './styles'
 
 export function Header() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova Transação
                  </button>
+                 <Modal isOpen={isNewTransactionModalOpen}>
+                     <h2>Cadastrar Transação</h2>
+                 </Modal>
             </Content>
-            
         </Container>
     )
 }
